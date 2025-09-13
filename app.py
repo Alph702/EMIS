2d0b16a17578c21311e2977729fdcac5acda0102
Alph702 <alph702.12@gmail.com>
2025-09-13 17:44:48 +0500

    added some data validation

--- app.py
+++ app.py
@@ -4,13 +4,13 @@ from bot import fill_form_from_excel
 import io
 import os
 
-df = pd.read_excel("data/template.xlsx")
+template = pd.read_excel("data/template.xlsx")
 
 st.title("EMIS Form Filler")
 
 output = io.BytesIO()
 with pd.ExcelWriter(output, engine='openpyxl') as writer:
-    df.to_excel(writer, index=False)
+    template.to_excel(writer, index=False)
 excel_data = output.getvalue()
 
 st.download_button(
@@ -26,27 +26,31 @@ uploaded_file = st.file_uploader("Choose an Excel file", type="xlsx")
 if uploaded_file is not None:
     data = pd.read_excel(uploaded_file)
     st.write(data)
+    if data.columns.tolist() == template.columns.tolist():
+        st.error("Columns in the Excel file do not match the template.", icon="🚨")
     
-    username = st.text_input("Username")
-    password = st.text_input("Password", type="password")
+        username = st.text_input("Username")
+        password = st.text_input("Password", type="password")
 
-    if username and password:
-        photos = st.file_uploader(
-            "Upload images", accept_multiple_files=True, type=["jpg", "png"]
-        )
+        if username and password:
+            photos = st.file_uploader(
+                "Upload images", accept_multiple_files=True, type=["jpg", "png"]
+            )
 
-        if photos:
-            save_dir = "Photos"
-            os.makedirs(save_dir, exist_ok=True)
-            for photo in photos:
-                save_path = os.path.join(save_dir, photo.name)
-                with open(save_path, "wb") as f:
-                    f.write(photo.getbuffer())
-            st.success(f"Saved {len(photos)} file(s) to: {save_dir}")
+            if photos:
+                save_dir = "Photos"
+                os.makedirs(save_dir, exist_ok=True)
+                for photo in photos:
+                    save_path = os.path.join(save_dir, photo.name)
+                    with open(save_path, "wb") as f:
+                        f.write(photo.getbuffer())
+                st.success(f"Saved {len(photos)} file(s) to: {save_dir}")
 
-            if st.button("Run"):
-                try:
-                    fill_form_from_excel(data, username, password)
-                    st.success("Form filling process completed successfully!")
-                except Exception as e:
-                    st.error(f"An error occurred {e}")
+                if st.button("Run"):
+                    try:
+                        fill_form_from_excel(data, username, password)
+                        st.success("Form filling process completed successfully!")
+                    except Exception as e:
+                        st.error(f"An error occurred {e}")
+    else:
+        st.error("Columns in the Excel file do not match the template.", icon="🚨")
